25b9b4a5f06b82016754d7a8dbb0394c834dc3af
Sergius Nahnoinyi <86116594+SergiusNahnoinyi@users.noreply.github.com>
2022-06-25 17:57:49 +0300

    Implements server errors handlers

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Express, Request, Response, NextFunction } from 'express';
 import logger from 'morgan';
 import cors from 'cors';
 import mongoose from 'mongoose';
@@ -6,7 +6,7 @@ import mongoose from 'mongoose';
 import { config } from './config/config';
 import router from './routes/articlesRouter';
 
-const app = express();
+const app: Express = express();
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 app.use(logger(formatsLogger));
@@ -15,15 +15,39 @@ app.use(express.json());
 
 app.use('/api/articles', router);
 
-mongoose.connect(config.mongo.url)
+mongoose
+  .connect(config.mongo.url)
   .then(() => {
     app.listen(config.server.port, function () {
       console.log(
-        `Database connection successfully. Use our API on port: ${config.server.port}`
+        `Database connection successfully. Use our API on port: ${config.server.port}`,
       );
     });
   })
-  .catch((err) => {
+  .catch(err => {
     console.log(`Server not running. Error message: ${err.message}`);
     process.exit();
   });
+
+app.use((req: Request, res: Response) => {
+  res.status(404).json({
+    status: 'Error',
+    code: 404,
+    message: `Use api on routes: 
+    GET /api/articles - get articles
+    POST /api/articles - add article
+    PUT /api/articles/:id - update article
+    DELETE /api/articles/:id - delete article`,
+    data: 'Not found',
+  });
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.log(err.stack);
+  res.status(500).json({
+    status: 'Fail',
+    code: 500,
+    message: err.message,
+    data: 'Internal Server Error',
+  });
+});
