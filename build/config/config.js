09bd858c562df3915ef8740ebb5307d99b53bec3
Sergius Nahnoinyi <86116594+SergiusNahnoinyi@users.noreply.github.com>
2022-07-01 10:48:58 +0300

    Makes come changes

--- build/config/config.js
+++ build/config/config.js
@@ -1,22 +1,24 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
 exports.config = void 0;
-const dotenv_1 = __importDefault(require("dotenv"));
+const dotenv_1 = __importDefault(require('dotenv'));
 dotenv_1.default.config();
-const PORT = process.env.SERVER_PORT;
+const PORT = process.env.SERVER_PORT || 4000;
 const USERNAME = process.env.DB_USERNAME;
 const PASSWORD = process.env.DB_PASSWORD;
 const URL = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.vmlka.mongodb.net/db-articles`;
 exports.config = {
-    mongo: {
-        username: USERNAME,
-        password: PASSWORD,
-        url: URL,
-    },
-    server: {
-        port: PORT,
-    },
+  mongo: {
+    username: USERNAME,
+    password: PASSWORD,
+    url: URL,
+  },
+  server: {
+    port: PORT,
+  },
 };
